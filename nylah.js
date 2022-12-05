4b6aa59f45574d4b8d7ef5d86c522057c9c0f242
nylaho <112510058+nylaho@users.noreply.github.com>
2022-12-05 17:38:48 -0500

    Add files via upload

---
+++ nylah.js
@@ -0,0 +1,10 @@
+// var audio = document.createElement("audio");
+// audio.src = "jcole.mp3";
+
+// audio.addEventListener("canplaythrough", function () {
+//         setTimeout(function(){
+//             audio.pause();
+//             alert("Audio Stop Successfully");
+//         },
+//         10000);
+// }, false); 
